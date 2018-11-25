957dda7265a94927262ca3863f16785265085fcf
Britny Edwards <bredwards2@gmail.com>
2018-11-25 23:31:57 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,7 +19,8 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-  shipping_manifest["oil painting"]
+   shipping_manifest = []
+   shipping_manifest["oil painting"]
 
 def adding
   shipping_manifest = {
